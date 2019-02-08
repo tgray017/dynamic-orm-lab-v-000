a4e82d8bd5cb49e41316e75940a8db50f65bd2f3
tgray017 <tgray017@gmail.com>
2019-02-08 12:49:20 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,16 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
   
+def self.table_name
+  self.to_s.downcase.pluralize
+end
+
+def self.column_names
+  sql = "pragma table_info('#{table_name}')"
+  DB[:conn].execute(sql).collect {|hash| hash["name"]}
+end
+
 end
--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,8 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+
+
+
+
 end
