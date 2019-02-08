1973edd9784df52f44465d5b702a0be0103e7426
tgray017 <tgray017@gmail.com>
2019-02-08 13:09:27 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,10 +3,7 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-  #attr_accessor :id, :name, :grade
 
-
-
-
+  self.column_names.each {|col_name| attr_accessor col_name.to_sym}
 
 end
