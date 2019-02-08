4a17c6f5b5b088c5e3e17b8cf536554fc100db5d
tgray017 <tgray017@gmail.com>
2019-02-08 13:08:05 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -13,4 +13,12 @@ def self.column_names
   DB[:conn].execute(sql).collect {|hash| hash["name"]}
 end
 
+self.column_names.each {|col_name| attr_accessor col_name.to_sym}
+
+def initialize(attributes = nil)
+  unless attributes.nil? 
+    attributes.each {|k, v| send("#{k}=", v)}
+  end
+end
+
 end
