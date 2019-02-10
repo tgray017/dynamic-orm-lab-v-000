0f4c7b7185ff91393604392f67c8472ef1e50637
tgray017 <tgray017@gmail.com>
2019-02-10 14:37:24 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -4,39 +4,51 @@ require 'pry'
 
 class InteractiveRecord
   
-def self.table_name
-  self.to_s.downcase.pluralize
-end
-
-def self.column_names
-  sql = "pragma table_info('#{table_name}')"
-  DB[:conn].execute(sql).collect {|hash| hash["name"]}
-end
-
-def initialize(attributes={})
-    attributes.each {|k, v| self.send("#{k}=", v)}
-end
-
-def table_name_for_insert
-  self.class.table_name
-end
-
-def col_names_for_insert
-  self.class.column_names.delete_if {|col| col == "id"}.join(", ")
-end
-
-def values_for_insert
-  values = []
-  self.class.column_names.each do |col_name|
-    values << "'#{send(col_name)}'" unless send(col_name).nil?
+  def self.table_name
+    self.to_s.downcase.pluralize
   end
-  values.join(", ")
-end
-
-def save
-  sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
-  DB[:conn].execute(sql)
-  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
-end
+  
+  def self.column_names
+    sql = "pragma table_info('#{table_name}')"
+    DB[:conn].execute(sql).collect {|hash| hash["name"]}
+  end
+  
+  def initialize(attributes={})
+      attributes.each {|k, v| self.send("#{k}=", v)}
+  end
+  
+  def table_name_for_insert
+    self.class.table_name
+  end
+  
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
+  
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
+  
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+  
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
+  
+  def self.find_by(attributes)
+    column = attributes.keys[0].to_s
+    value = attributes.values[0].to_s
+    sql = "SELECT * FROM #{self.table_name} WHERE #{column} = '#{value}'"
+    DB[:conn].execute(sql)
+  end
 
 end
